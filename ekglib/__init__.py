a56494bbaa46a84e2e8ada6f4656c4a07467d08d
Stefan Szaniszlo <stefan.szaniszlo@agnos.ai>
2020-11-19 10:05:45 +0000

    update version

--- ekglib/__init__.py
+++ ekglib/__init__.py
@@ -2,7 +2,7 @@
 """
 
 # The format of the __version__ line is matched by a regex in setup.py
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __all__ = [  # noqa: F405
     'concept_parser',
     'data_source',

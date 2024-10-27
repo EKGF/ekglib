d9956da3c93bfb75936179204377dc580ee63234
Jacobus Geluk <jacobus.geluk@ekgf.org>
2024-10-27 14:57:13 +0000

    feat(build): switched from poetry to uv

--- run-tests.sh
+++ run-tests.sh
@@ -2,112 +2,24 @@
 #
 # run the python tests
 #
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-VIRTUAL_ENV="${SCRIPT_DIR}/.venv"
+GIT_ROOT="$(git rev-parse --show-toplevel)"
+VIRTUAL_ENV="${GIT_ROOT}/.venv"
 
-python_version=3.10.8
+python_version="$(cat "${GIT_ROOT}/.python-version")"
 
 flag_file="/tmp/ekglib-last-checked-environment.flag"
 
-#
-# Returns true (0) if the python environment should be checked (which takes quite some time,
-# we want to be able to continuously run run-test.sh during a development cycle because
-# doing it from IntelliJ or other IDEs is not in all cases the easiest option)
-#
-function shouldCheckEnvironment() {
-  #
-  # If the flag file doesn't exist yet return true
-  #
-  [[ -f "${flag_file}" ]] || return 0
-  #
-  # If the flag file is older than X minutes then return true
-  #
-  find "${flag_file}" -mmin +5 -type f | grep -q flag && return 0
-  #
-  # If you added new dependencies then return true
-  #
-  [[ "${SCRIPT_DIR}/Pipfile" -nt "${flag_file}" ]] && return 0
-  #
-  # Add whatever check here that you think should trigger a recheck of the environment
-  #
-  return 1
-}
-
 function checkEnvironment() {
 
-  cd "${SCRIPT_DIR}" || return $?
+  cd "${GIT_ROOT}" || return $?
 
   echo "========================== check environment"
 
-#if ! shouldCheckEnvironment ; then
-#    echo "Skipping environment check"
-#    return 0
-#  fi
-
   touch "${flag_file}" || return 1
 
-  # shellcheck disable=SC2035
-  rm -f *.whl >/dev/null 2>&1
+  "${GIT_ROOT}/setup.sh" || return 1
 
-  if command -v apt-get >/dev/null 2>&1 ; then
-    sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
-      libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-      xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
-  fi
-
-  if ! command -v asdf >/dev/null 2>&1 ; then
-    if [[ "$(uname)" == "Darwin" ]] ; then
-      brew install asdf
-    fi
-  fi
-
-  if command -v asdf >/dev/null 2>&1 ; then
-    asdf plugin-add python # takes no time when it's already installed
-    if ! asdf install python ${python_version} ; then # takes no time when it's already installed
-      echo "ERROR: Could not install python ${python_version}"
-      return 1
-    fi
-    asdf local python ${python_version}
-    #
-    # See https://gist.github.com/rubencaro/888fb8e4f0811e79fa22b5ac39610c9e#setup-a-new-project
-    #
-    asdf exec python3 -m venv --upgrade --upgrade-deps "${VIRTUAL_ENV}"
-    source "${VIRTUAL_ENV}/bin/activate"
-  else
-    echo "ERROR: Please install asdf"
-    return 1
-  fi
-
-  if ! command -v "${VIRTUAL_ENV}/bin/python3" >/dev/null 2>&1 ; then
-    echo "ERROR: You don't have ${VIRTUAL_ENV}/bin/python3"
-    return 1
-  fi
-
-  if ! command -v "${VIRTUAL_ENV}/bin/pip3" >/dev/null 2>&1 ; then
-    echo "ERROR: You don't have ${VIRTUAL_ENV}/bin/pip3"
-    return 1
-  fi
-
-  "${VIRTUAL_ENV}/bin/pip3" install --upgrade pip poetry wheel setuptools
-  "${VIRTUAL_ENV}/bin/pip3" install flake8 pytest pytest-cov
-  "${VIRTUAL_ENV}/bin/poetry" config virtualenvs.in-project true
-  "${VIRTUAL_ENV}/bin/poetry" install
-  "${VIRTUAL_ENV}/bin/poetry" build
-
-  if ! command -v "${VIRTUAL_ENV}/bin/flake8" >/dev/null 2>&1 ; then
-    echo "ERROR: You don't have ${VIRTUAL_ENV}/bin/flake8"
-    return 1
-  fi
-
-  if ! command -v "${VIRTUAL_ENV}/bin/pytest" >/dev/null 2>&1 ; then
-    echo "ERROR: You don't have ${VIRTUAL_ENV}/bin/pytest"
-    return 1
-  fi
-
-  # shellcheck disable=SC2035
-  rm -f *.whl >/dev/null 2>&1
-
-  return 0
+  source "${VIRTUAL_ENV}/bin/activate"
 }
 
 function runLint() {
@@ -123,29 +35,13 @@ function runLint() {
   return 0
 }
 
-#
-# Create the .test directory (which is git ignored) with some easy to parse files
-# that specify things like the LDAP naming context to use for tests etc.
-# This way you can test against your local company server without revealing any
-# company names and other confidential information ending up in git.
-#
-function createTestInfoFiles() {
-
-  mkdir -p "${SCRIPT_DIR}/.test" >/dev/null 2>&1
-
-  if [[ ! -f "${SCRIPT_DIR}/.test/ldap-domain" ]] ; then
-    echo "your-kompany.kom" > "${SCRIPT_DIR}/.test/ldap-domain"
-  fi
-
-  return 0
-}
 
 function runTests() {
 
   local -r testWildCard="$*"
   local opts=""
 
-  cd "${SCRIPT_DIR}" || return $?
+  cd "${GIT_ROOT}" || return $?
 
   if [[ -n "${testWildCard}" ]] ; then
     opts+="-k ${testWildCard}"
@@ -154,13 +50,7 @@ function runTests() {
   echo "========================== tests"
 
   # shellcheck disable=SC2086
-  "${VIRTUAL_ENV}/bin/pytest" tests/ -rA \
-    --doctest-modules \
-    --junitxml=junit/test-results.xml \
-    --cov-report=html \
-    --maxfail=2 \
-    --showlocals \
-    --tb=short ${opts}
+  "${VIRTUAL_ENV}/bin/pytest" tests ${opts}
   local -r rc=$?
   echo "pytest ended with rc=${rc}"
   return ${rc}
@@ -168,7 +58,6 @@ function runTests() {
 
 checkEnvironment || exit 1
 runLint || exit 1
-createTestInfoFiles || exit 1
 # shellcheck disable=SC2068
 runTests $@
 exit $?
---
+++ setup.sh
@@ -0,0 +1,185 @@
+#!/usr/bin/env bash
+#
+# This script is used to set up the environment for the project.
+# It installs the required tools and packages and sets up the virtual environment.
+#
+# Usage: ./setup.sh
+#
+REPO_NAME="ekgf/ekglib"
+GIT_ROOT="$(git rev-parse --show-toplevel)"
+GIT_BRANCH="$(git name-rev --name-only HEAD)"
+PROJECT_TMP_DIR="/tmp/ekglib/${GIT_BRANCH}/"
+ACTUAL_VENV_DIR="${PROJECT_TMP_DIR}/.venv"
+LOCAL_VENV_DIR="${GIT_ROOT}/.venv"
+UV_PROJECT_ENVIRONMENT=".venv"
+
+green_bold="\033[1;32m"
+gray_bold="\033[1;30m"
+blue_bold="\033[1;34m"
+magenta_bold="\033[1;35m"
+red_bold="\033[1;31m"
+end_color="\033[0m"
+
+function heading() {
+    echo -e "${green_bold}>>>>>>> $* <<<<<<<${end_color}"
+}
+
+function subheading() {
+    echo -e "${gray_bold}>>>>>>> $* <<<<<<<${end_color}"
+}
+
+function check_system() {
+    if ! command -v python3 &> /dev/null ; then
+        echo -e "${red_bold}ERROR: Python 3 is not installed or not on your PATH.${end_color}" >&2
+        return 1
+    fi
+
+    return 0
+}
+
+function clean() {
+    heading "Cleaning up"
+
+    if [[ -d "${PROJECT_TMP_DIR}" ]] ; then
+        rm -rf ${PROJECT_TMP_DIR}
+    fi
+    if [[ -d "${ACTUAL_VENV_DIR}" ]] ; then
+        rm -rf ${ACTUAL_VENV_DIR}
+    fi
+    if [[ -d "${LOCAL_VENV_DIR}" ]] ; then
+        rm -rf ${LOCAL_VENV_DIR}
+    fi
+
+    return 0
+}
+
+function recreate_venv() {
+    heading "Recreating virtual environment"
+
+    if [[ ! -d "${ACTUAL_VENV_DIR}" ]] ; then
+        mkdir -p "${ACTUAL_VENV_DIR}" || return $?
+    fi
+    ln -sfn "${ACTUAL_VENV_DIR}" "${LOCAL_VENV_DIR}" || return $?
+    if [[ -d "${LOCAL_VENV_DIR}" ]] ; then
+        rm -rf "${LOCAL_VENV_DIR}" || return $?
+    fi
+
+    # First install a standard virtual environment using the system python
+    # so that we can then install uv as a tool into it and run that to
+    # recreate a version of the virtual environment that uv likes.
+    python3 -m venv --clear --system-site-packages "${LOCAL_VENV_DIR}" || return $?
+
+    if [[ ! -x "${LOCAL_VENV_DIR}/bin/pip3" ]] ; then
+        echo -e "${red_bold}ERROR: Could not create the virtual environment, pip3 is not executable.${end_color}" >&2
+        return 1
+    fi
+
+    "${LOCAL_VENV_DIR}/bin/pip3" install --upgrade pip || return $?
+    "${LOCAL_VENV_DIR}/bin/pip3" install --upgrade uv || return $?
+
+    # Now recreate the virtual environment using uv
+    "${LOCAL_VENV_DIR}/bin/uv" venv --allow-existing --system-site-packages "${LOCAL_VENV_DIR}" || return $?
+
+    if [[ ! -x "${LOCAL_VENV_DIR}/bin/uv" ]] ; then
+        echo -e "${red_bold}ERROR: Could not create the virtual environment, uv is not executable.${end_color}" >&2
+        return 1
+    fi
+
+    return 0
+}
+
+function install_stuff() {
+    heading "Install stuff"
+
+    if command -v apt-get >/dev/null 2>&1 ; then
+        sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+            libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+            xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+    fi
+
+    # Install the dependencies using uv, which will also install all the tools
+    # mentioned under [dependency-groups.dev] in the pyproject.toml file.
+    subheading "uv sync"
+    "${LOCAL_VENV_DIR}/bin/uv" sync || return $?
+
+    return 0
+}
+
+#
+# Create the .test directory (which is git ignored) with some easy to parse files
+# that specify things like the LDAP naming context to use for tests etc.
+# This way you can test against your local company server without revealing any
+# company names and other confidential information ending up in git.
+#
+function prepare_tests() {
+    heading "Preparing for tests"
+
+    mkdir -p "${GIT_ROOT}/.test" >/dev/null 2>&1
+
+    if [[ ! -f "${GIT_ROOT}/.test/ldap-domain" ]] ; then
+        echo "your-kompany.kom" > "${GIT_ROOT}/.test/ldap-domain"
+    fi
+
+    return 0
+}
+
+function move_cache() {
+    local -r local_path="${1}"
+    local -r real_path="${2}"
+
+    if [[ -L "${local_path}" && -d "${local_path}" ]] ; then
+        echo -r "${green_bold}${local_path}${end_color} is already a symlink to ${green_bold}$(realpath ${real_path})${end_color}"
+    else
+        if [[ ! -d "${real_path}" ]] ; then
+            mkdir -p "${real_path}" || return $?
+        fi
+        ln -sfn "${real_path}" "${local_path}" || return $?
+    fi
+
+    return 0
+}
+
+# We need to move the bulky stuff to /tmp to avoid running out of space
+function move_caches() {
+    heading "Moving caches to /tmp"
+
+    move_cache "${LOCAL_VENV_DIR}" "${ACTUAL_VENV_DIR}" || return $?
+    move_cache "${GIT_ROOT}/.tox" "${PROJECT_TMP_DIR}/.tox" || return $?
+    move_cache "${GIT_ROOT}/.mypy_cache" "${PROJECT_TMP_DIR}/.mypy_cache" || return $?
+    move_cache "${GIT_ROOT}/.pytest_cache" "${PROJECT_TMP_DIR}/.pytest_cache" || return $?
+    move_cache "${GIT_ROOT}/.ruff_cache" "${PROJECT_TMP_DIR}/.ruff_cache" || return $?
+
+    return 0
+}
+
+function last_message() {
+    heading "Setup complete"
+
+    echo -e "Your environment has been set up successfully, you can now use commands like these:"
+    echo -e " - ${blue_bold}source ${LOCAL_VENV_DIR}/bin/activate${end_color}"
+    echo -e " - ${blue_bold}uv sync${end_color} to install the dependencies"
+    echo -e " - ${blue_bold}tox${end_color} or ${blue_bold}uv run tox${end_color} to run all jobs like tests, linting, etc."
+    echo -e " - ${blue_bold}pytest${end_color} or ${blue_bold}uv run pytest${end_color} to run tests"
+    echo -e " - ${blue_bold}ruff check${end_color} or ${blue_bold}uv run ruff check${end_color} to run the ruff linter"
+    echo ""
+    echo "NOTE: Running these commands outside the context of an active virtual environment requires ~/.local/bin to be in your PATH"
+
+    return 0
+}
+
+function main() {
+    heading "Setting up environment for ${REPO_NAME}"
+
+    check_system || return $?
+    clean || return $?
+    recreate_venv || return $?
+    install_stuff || return $?
+    move_caches || return $?
+    prepare_tests || return $?
+    last_message || return $?
+
+    return 0
+}
+
+main
+exit $?
